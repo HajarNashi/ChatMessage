6c37a4900a35bd1e17e9acdf9b508a0a4f019c47
HajarNashi <103394242+HajarNashi@users.noreply.github.com>
2023-08-10 21:53:43 +0300

    Initial Commit

---
+++ LiveChat/LiveChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChatApp.swift
+//  LiveChat
+//
+//  Created by Hajar Nashi on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
